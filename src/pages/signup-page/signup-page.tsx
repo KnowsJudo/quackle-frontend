4cd61739fee1bfd2e86b9550c00776fcc2b47b6c
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-10-24 13:26:15 +1100

    handler

--- src/pages/signup-page/signup-page.tsx
+++ src/pages/signup-page/signup-page.tsx
@@ -1,41 +1,52 @@
-import React from 'react';
-import axios from 'axios';
-import './signup-page.css';
+import React, { useState } from "react";
+import axios from "axios";
+import { IUserDetails } from "../../types/user-types";
+import "./signup-page.css";
 
 export const SignUpPage: React.FC = () => {
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+  const [userDetails, setUserDetails] = useState<IUserDetails>({
+    name: "",
+    username: "",
+    password: "",
+    email: "",
+  });
+
+  const handleChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+    field: string,
+  ) => {
     event.preventDefault();
-    console.log(event.target.value);
+    setUserDetails({ ...userDetails, [field]: event.target.value });
   };
 
   const signUp = () => {
     axios
       .post(
-        '//localhost:3001/api/user',
+        "//localhost:3001/api/user",
         {
-          name: 'Tom',
-          username: 'BKHZ',
-          password: 'tom123',
-          email: 'tomfjohn@hotmail.com',
+          name: userDetails.name,
+          username: userDetails.username,
+          password: userDetails.password,
+          email: userDetails.email,
         },
         {
           headers: {
-            'Content-Type': 'application/json',
+            "Content-Type": "application/json",
           },
         },
       )
-      .then((res) => console.log(res.data, 'users'))
+      .then((res) => console.log(res.data, "users"))
       .catch((e) => console.error(e));
   };
 
   const login = () => {
     axios
-      .get('//localhost:3001/api/user', {
+      .get("//localhost:3001/api/user", {
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
         },
       })
-      .then((res) => console.log(res.data, 'users'))
+      .then((res) => console.log(res.data, "users"))
       .catch((e) => console.error(e));
   };
 
@@ -44,7 +55,40 @@ export const SignUpPage: React.FC = () => {
       <section className="signup-section">
         <h1>Quackle</h1>
         <h3>Join the avian world&apos;s largest social network</h3>
-        <input onChange={(e) => handleChange(e)}></input>
+        <form className="signup-form">
+          <label>
+            Name
+            <input
+              placeholder="Name"
+              onChange={(e) => handleChange(e, "name")}
+              value={userDetails.name}
+            ></input>
+          </label>
+          <label>
+            Username
+            <input
+              placeholder="Username"
+              onChange={(e) => handleChange(e, "username")}
+              value={userDetails.username}
+            ></input>
+          </label>
+          <label>
+            Password
+            <input
+              placeholder="Password (min. 7 characters)"
+              onChange={(e) => handleChange(e, "password")}
+              value={userDetails.password}
+            ></input>
+          </label>
+          <label>
+            Email
+            <input
+              placeholder="Email address"
+              onChange={(e) => handleChange(e, "email")}
+              value={userDetails.email}
+            ></input>
+          </label>
+        </form>
         <button onClick={() => signUp()}>Sign up</button>
         <h5>Existing user?</h5>
         <button onClick={() => login()}>LOGIN</button>
---
+++ src/types/user-types.ts
@@ -0,0 +1,6 @@
+export interface IUserDetails {
+  name: string;
+  username: string;
+  password: string;
+  email: string;
+}
