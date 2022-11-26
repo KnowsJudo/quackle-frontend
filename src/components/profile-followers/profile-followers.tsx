674f4510e1dad40b345814c162f41515945ee196
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-11-26 15:58:53 +1100

    css

--- src/components/profile-followers/profile-followers.tsx
+++ src/components/profile-followers/profile-followers.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./profile-followers.css";
 
 export const ProfileFollowers: React.FC = () => {
   return <section className="profile-followers">Followers:</section>;
--- src/components/profile-following/profile-following.tsx
+++ src/components/profile-following/profile-following.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "./profile-following.css";
 
 export const ProfileFollowing: React.FC = () => {
   return <section className="profile-following">Following Users:</section>;
