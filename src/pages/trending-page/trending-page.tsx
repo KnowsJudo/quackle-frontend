fa3420520e822ee33d9e60adcb523975f1cc69fa
Lachlan Bardwell <75568181+KnowsJudo@users.noreply.github.com>
2022-12-28 22:29:12 +1100

    trend

--- src/pages/trending-page/trending-page.tsx
+++ src/pages/trending-page/trending-page.tsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ProfileSideBar } from "../../components/profile-sidebar/profile-sidebar";
+import { ProfileUser } from "../../components/profile-user/profile-user";
+import { QuackleContext } from "../../context/user-context";
 import "./trending-page.css";
 
 export const TrendingPage: React.FC = () => {
-  return <div className="trending-container"></div>;
+  const { userData, setInitiateQuack } = useContext(QuackleContext);
+
+  return (
+    <div className="trending-container">
+      <ProfileUser
+        loggedIn={userData.username ? true : false}
+        setInitiateQuack={setInitiateQuack}
+      />
+      <ProfileSideBar loggedIn={userData.username ? true : false} />
+    </div>
+  );
 };
